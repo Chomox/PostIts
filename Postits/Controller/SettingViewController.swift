a8eff36f671ac583f43760dbb9332835ff6098fa
Moto <mott.eng0124@gmail.com>
2021-09-07 02:37:47 +0900

    Create SettingViewController

---
+++ Postits/Controller/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  Postits
+//
+//  Created by Motoyuki Ito on 2021/09/07.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
